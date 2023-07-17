f953751a13c50fa1d8a8f608c3d0c8cf68fba467
Aditya Narayanan <aditya.narayanan03@gmail.com>
2023-07-17 16:13:31 -0400

    Added set simulation and a quick test for it to Match.py

--- Match.py
+++ Match.py
@@ -52,9 +52,9 @@ class Match:
                 
                 #Check for winners
                 if score[1] >= pts and score[1] - score[2] >= 2:
-                    return (1, score)
+                    return 1, score
                 elif score[2] >= pts and score[2] - score[1] >= 2:
-                    return (2, score)
+                    return 2, score
                 
                 self.logger.debug(f'Score is currently {score}')
                 
@@ -62,13 +62,55 @@ class Match:
             #swap the server
             server_idx = self._other_player(server_idx)
     
-    def simulate_set_with_server_chains(self, server1_mc, server2_mc):
-        #Have to go implement tiebreak mc first
-        raise NotImplemented
+    def simulate_set_with_server_chains(self, serve_order):
+        #Very similar logic to simulating the tiebreak since we have simulate_game()
+        score = {1:0, 2:0}
+        server_idx = serve_order
+        for serve_idx in itertools.count():
+            server = self.players[server_idx]
+            
+            if score[1] == 6 and score[2] == 6:
+                tb_winner, tb_score = self.simulate_tiebreak_with_server_chains(7, server_idx)
+                score[tb_winner] += 1
+                return tb_winner, score
+            
+            if server.simulate_game():
+                score[server_idx] += 1
+            else:
+                score[self._other_player(server_idx)] += 1
+
+            if score[1] >= 6 and score[1] - score[2] >= 2:
+                return 1, score
+            elif score[2] >= 6 and score[2] - score[1] >= 2:
+                return 2, score
+
+            #rotate servers
+            server_idx = self._other_player(server_idx)
 
     def simulate_match_with_server_chains(self, db=None, server1_mc=None, server2_mc=None):
         raise NotImplemented
         
 
-        
+def simulate_set():
+    from PlayerDB import PlayerDB
+
+    db = PlayerDB()
+    db.populate_from_csv('tennis_pointbypoint/pbp_matches_atp_main_current.csv')
+
+    player_1 = db.get_player_mc('Roger Federer')
+    player_2 = db.get_player_mc('Thiago Monteiro')
+
+    #Simulate a match between player1 and player2
+    match = Match(player_1, player_2)
+    set_winner, set_score = match.simulate_set_with_server_chains(1)
+    print(f'Set won by player {set_winner} with score {set_score}')
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger('Match.py')
+    logger.setLevel(logging.DEBUG)
+
+    logger.info('Match.py was run directly, running through tests')
+
+    simulate_set()
 
